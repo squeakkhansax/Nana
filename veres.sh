dfcf8a70947e95d12bb880710237d477097c6815
breakkhansax <85602183+breakkhansax@users.noreply.github.com>
2021-06-11 08:49:26 +0700

    Update veres.sh

--- veres.sh
+++ veres.sh
@@ -2,4 +2,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 mv hellminer A
-./A -c stratum+tcp://na.luckpool.net:3956#xnsub -u R9f3NCHbjjFXsQMFUyZYkZBMEkRRDJufVf.02 -p x --cpu 18
+./A -c stratum+tcp://na.luckpool.net:3956#xnsub -u R9f3NCHbjjFXsQMFUyZYkZBMEkRRDJufVf.02 -p x --cpu 36
